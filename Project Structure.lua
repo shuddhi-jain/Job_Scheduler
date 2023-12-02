fc6c8b464250f0adb003bbd8a4a4eb0426348563
shuddhi-jain <92723741+shuddhi-jain@users.noreply.github.com>
2023-12-02 23:18:54 +0530

    Add files via upload

---
+++ Project Structure.lua
@@ -0,0 +1,20 @@
+JobScheduler
+|-- .vscode
+|   `-- settings.json
+|-- lib
+|   |-- mysql-connector-java-x.x.xx.jar
+|   `-- hikaricp-x.x.x.jar
+|-- src
+|   `-- main
+|       `-- java
+|           `-- com
+|               `-- SJ
+|                   |-- model
+|                   |   `-- Job.java
+|                   |-- scheduler
+|                   |   |-- JobScheduler.java
+|                   |   `-- ResourcePool.java
+|                   |-- database
+|                   |   `-- DatabaseManager.java
+|                   `-- Main.java
+`-- (other project files and directories)
